ef9012221396a1a008b1275d613b2b60cd381f61
Amy <wilford.amy@gmail.com>
2022-10-11 17:00:50 -0400

    drafted test files

---
+++ tests/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee');
+
+describe('initialization', ()=> {
+    it('Verifies a new employee will be an object', ()=>{
+        const employee = new Employee();
+        expect(typeof(employee)).toBe('object');
+    });
+
+    it('Confirms employee name from provided argument', ()=>{
+        const name = 'Emily'
+        const employee = new Employee(name);
+        expect(employee.name).toBe(name);
+    });
+    it('Confirms emplyee ID from provided argument', ()=>{
+        const id = 01;
+        const employee = new Employee('Emily', id);
+        expect(employee.id).toBe(id);
+    })
+    it('Confirms employee email from provided argument', ()=>{
+        const email = 'emily@test.com';
+        const employee = new Employee('Emily', 01, email);
+        expect(employee.email).toBe(email);
+    })
+})
